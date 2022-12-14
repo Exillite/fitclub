855fdd3aaa4d658d8e603268024cfa3c9906897f
Exillite <greencod67@gmail.com>
2022-12-14 11:37:29 +0300

    translate admin panel

--- app/fitclub/admin.py
+++ app/fitclub/admin.py
@@ -1,12 +1,121 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(SportGroup)
-admin.site.register(GroupTime)
-admin.site.register(Trening)
-admin.site.register(Client)
-admin.site.register(Param)
-admin.site.register(Peyment)
-admin.site.register(Spending)
-admin.site.register(Salary)
-admin.site.register(Income)
+class SportGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user_view')
+    list_filter = ('trener',)
+    search_fields = ('name',)
+    def user_view(self, obj):
+        return f"{obj.trener.first_name} {obj.trener.last_name}"
+
+    user_view.short_description = "Тренер"
+
+class TreningAdmin(admin.ModelAdmin):
+    list_display = ('day', 'start', 'end', 'trening_type_view', 'clients_view', 'user_view', 'is_helper_view')
+    search_fields = ('day',)
+    list_filter = ('trening_type',)
+    
+    def user_view(self, obj):
+        return f"{obj.trener.first_name} {obj.trener.last_name}"
+    
+    def trening_type_view(self, obj):
+        if obj.trening_type == "group":
+            return 'Групповое занятие'
+        else:
+            return 'Индивидуальное занятие'
+    
+    def clients_view(self, obj):
+        if obj.trening_type == "group":
+            return f"Группа: {obj.group.name}"
+        else:
+            client = obj.clients.first()
+            return f"{client.name} {client.surname}"
+    
+    def is_helper_view(self, obj):
+        if obj.helper:
+            return True
+        else:
+            return False
+    
+    trening_type_view.short_description = "Тип"
+    clients_view.short_description = "Участники"
+    user_view.short_description = "Тренер"
+    is_helper_view.short_description = "Есть ли помощник"
+    is_helper_view.boolean = True
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('fio_view', 'phone', 'email')
+    search_fields = ('name', 'surname', 'email', 'phone')
+    def fio_view(self, obj):
+        return f"{obj.name} {obj.surname}"
+    
+    fio_view.short_description = "Имя Фамилия"
+  
+class ParamAdmin(admin.ModelAdmin):
+    list_display = ('title_view', 'value')
+    
+    def title_view(self, obj):
+        if obj.title:
+            return obj.title
+        else:
+            return obj.key
+
+
+class PeymentAdmin(admin.ModelAdmin):
+    list_display = ('fio_view', 'date', 'value', 'pay_type_view', 'way_view')
+    
+    def fio_view(self, obj):
+        return f"{obj.client.name} {obj.client.surname}"
+    
+    def pay_type_view(self, obj):
+        if obj.pay_type == "one":
+            return "Индивидуальное занятие"
+        elif obj.pay_type == "one_month":
+            return "Абонимент на индивидуальное занятие"
+        elif obj.pay_type == "group":
+            return "Групповое занятие"
+        elif obj.pay_type == "group_month":
+            return "Абонимент на групповые занятия"
+        
+    def way_view(self, obj):
+        if obj.way == "card":
+            return "Безналичная оплата"
+        else:
+            return "Наличная оплата"
+    
+    fio_view.short_description = "Ученик"
+    pay_type_view.short_description = "Тип оплаты"
+    way_view.short_description = "Способ оплаты"
+
+
+class SpendingAdmin(admin.ModelAdmin):
+    list_display = ('date', 'key', 'value')
+    list_display_links = ('date', 'key')
+    search_fields = ('key', 'value')
+
+
+class SalaryAdmin(admin.ModelAdmin):
+    list_display = ('user_view', 'date', 'give')
+    
+    def user_view(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+
+    user_view.short_description = "Сотрудник"
+
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('date', 'key', 'value')
+    list_display_links = ('date', 'key')
+    search_fields = ('key', 'value')
+
+
+admin.site.register(SportGroup, SportGroupAdmin)
+# admin.site.register(GroupTime)
+admin.site.register(Trening, TreningAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Param, ParamAdmin)
+admin.site.register(Peyment, PeymentAdmin)
+admin.site.register(Spending, SpendingAdmin)
+admin.site.register(Salary, SalaryAdmin)
+admin.site.register(Income, IncomeAdmin)
--- app/fitclub/models.py
+++ app/fitclub/models.py
@@ -3,12 +3,17 @@ from django.conf import settings
 
 
 class SportGroup(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Название")
     trener = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        null=True
+        null=True,
+        verbose_name="Тренер"
     )
+    
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
 
     def __str__(self):
@@ -16,55 +21,75 @@ class SportGroup(models.Model):
 
 
 class GroupTime(models.Model):
-    start = models.TimeField()
-    end = models.TimeField()
-    day = models.IntegerField() # 1 - 7
+    start = models.TimeField(verbose_name="Начало")
+    end = models.TimeField(verbose_name="Конец")
+    day = models.IntegerField(verbose_name="День недели") # 1 - 7
 
-    group = models.ForeignKey('SportGroup', on_delete=models.PROTECT, null=True)
-    
+    group = models.ForeignKey('SportGroup', on_delete=models.PROTECT, null=True, verbose_name="Группа")
+        
+    class Meta:
+        verbose_name = 'Время занятий'
+        verbose_name_plural = 'Время занятий'
+
     def __str__(self):
         return f"{self.group.name}: {self.start} - {self.end}"
 
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255, null=True, blank=True)
-    email = models.CharField(max_length=255, null=True, blank=True)
-    groups = models.ManyToManyField(SportGroup)
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    surname = models.CharField(max_length=255, verbose_name="Фамилия")
+    phone = models.CharField(max_length=255, null=True, blank=True, verbose_name="Телефон")
+    email = models.CharField(max_length=255, null=True, blank=True, verbose_name="e-mail")
+    groups = models.ManyToManyField(SportGroup, verbose_name="Группы")
+    
+    class Meta:
+        verbose_name = 'Ученик'
+        verbose_name_plural = 'Ученики'
+    
+    def __str__(self):
+        return f"{self.name} {self.surname}"
 
 
 class Trening(models.Model):
-    start = models.TimeField()
-    end = models.TimeField()
-    day = models.DateField()
-    group = models.ForeignKey('SportGroup', on_delete=models.PROTECT, null=True, blank=True)
-    trening_type = models.CharField(max_length=50) # "personal" / "group"
-    col = models.IntegerField() # количество участников
-    is_was = models.BooleanField() # Была ло занятие
-    progul = models.BooleanField() # Был ли прогул
-    clients = models.ManyToManyField(Client, blank=True)
+    start = models.TimeField(verbose_name="Начало")
+    end = models.TimeField(verbose_name="Конец")
+    day = models.DateField(verbose_name="Дата")
+    group = models.ForeignKey('SportGroup', on_delete=models.PROTECT, null=True, blank=True, verbose_name="Группа")
+    trening_type = models.CharField(max_length=50, verbose_name="Тип занятия") # "personal" / "group"
+    col = models.IntegerField(verbose_name="Кол. участников") # количество участников
+    is_was = models.BooleanField(verbose_name="Было ли занятие") # Была ло занятие
+    progul = models.BooleanField(verbose_name="Прогул") # Был ли прогул
+    clients = models.ManyToManyField(Client, blank=True, verbose_name="Участники")
     trener = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name='royal_trainer'
+        related_name='royal_trainer',
+        verbose_name="Тренер"
     )
     helper = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name='helper_for_training'
+        related_name='helper_for_training',
+        verbose_name="Помощник"
     )
+    
+    class Meta:
+        verbose_name = 'Занитие'
+        verbose_name_plural = 'Занятия'
+    
+    def __str__(self):
+        return f"Зантие {self.day}: {self.start.hour}:{self.start.minute}"
 
 
 class Param(models.Model):
-    key = models.CharField(max_length=150)
-    value = models.IntegerField(null=True, blank=True)
-    title = models.CharField(max_length=255, blank=True)
+    key = models.CharField(max_length=150, verbose_name="Ключ")
+    value = models.IntegerField(null=True, blank=True, verbose_name="Значение")
+    title = models.CharField(max_length=255, blank=True, verbose_name="Описание")
 
 
     """
@@ -81,6 +106,10 @@ class Param(models.Model):
         sum_from_single   -- когда из группы пришёл один
     
     """
+    
+    class Meta:
+        verbose_name = 'Параметр'
+        verbose_name_plural = 'Параметры'
 
     def __str__(self):
         if self.title != "":
@@ -88,13 +117,19 @@ class Param(models.Model):
         return f"{self.key}"
 
 class Peyment(models.Model):
-    client = models.ForeignKey('Client', on_delete=models.PROTECT)
-    group = models.ForeignKey('SportGroup', on_delete=models.PROTECT, null=True, blank=True)
-    way = models.CharField(max_length=150) # "cash" / "card"
-    pay_type = models.CharField(max_length=150)
-    date = models.DateField(null=True, blank=True) 
-    value = models.IntegerField()
+    client = models.ForeignKey('Client', on_delete=models.PROTECT, verbose_name="Ученик")
+    group = models.ForeignKey('SportGroup', on_delete=models.PROTECT, null=True, blank=True, verbose_name="Группа")
+    way = models.CharField(max_length=150, verbose_name="Способ оплаты") # "cash" / "card"
+    pay_type = models.CharField(max_length=150, verbose_name="Тип оплаты")
+    date = models.DateField(null=True, blank=True, verbose_name="Дата") 
+    value = models.IntegerField(verbose_name="Сумма")
 
+    class Meta:
+        verbose_name = 'Платёж'
+        verbose_name_plural = 'Платежи'
+    
+    def __str__(self):
+        return f"{self.client}- {self.value}₽"
 
 """
 
@@ -107,10 +142,17 @@ class Peyment(models.Model):
 
 
 class Spending(models.Model):
-    key = models.CharField(max_length=255)
-    value = models.IntegerField()
-    spend_type = models.CharField(max_length=150)
-    date = models.DateField(null=True, blank=True)
+    key = models.CharField(max_length=255, verbose_name="Описание")
+    value = models.IntegerField(verbose_name="Сумма")
+    spend_type = models.CharField(max_length=150, verbose_name="Тип")
+    date = models.DateField(null=True, blank=True, verbose_name="Дата")
+    
+    class Meta:
+        verbose_name = 'Расход'
+        verbose_name_plural = 'Расходы'
+    
+    def __str__(self):
+        return f"{self.key}- {self.value}₽"
 
 class Salary(models.Model):
     user = models.ForeignKey(
@@ -118,14 +160,29 @@ class Salary(models.Model):
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name='who_trainer'
+        related_name='who_trainer',
+        verbose_name="Сотрудник"
     )
-    date = models.DateField()
+    date = models.DateField(verbose_name="Дата")
     accure = models.IntegerField(blank=True)
-    give = models.IntegerField()
+    give = models.IntegerField(verbose_name="Выплачено")
+    
+    class Meta:
+        verbose_name = 'Выплата'
+        verbose_name_plural = 'Зарплаты'
+    
+    def __str__(self):
+        return f"{self.user.first_name[0]}. {self.user.last_name} - {self.give}₽"
 
 
 class Income(models.Model):
-    key = models.CharField(max_length=255)
-    value = models.IntegerField()
-    date = models.DateField(null=True, blank=True)
+    key = models.CharField(max_length=255, verbose_name="Описание")
+    value = models.IntegerField(verbose_name="Сумма")
+    date = models.DateField(null=True, blank=True, verbose_name="Дата")
+    
+    class Meta:
+        verbose_name = 'Доход'
+        verbose_name_plural = 'Доходы'
+    
+    def __str__(self):
+        return f"{self.key}- {self.value}₽"
