1f32a194c1ae418265bbc26ecc441dfbc7d046aa
Exillite <greencod67@gmail.com>
2023-01-29 11:19:56 +0300

    sdskan kasnfgkl anjkn alfg

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ docker-compose -f docker-compose.prod.yml up -d --build
 docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
 docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
 
-docker exec -itd fitclub-web-1 python manage.py bot
-docker exec -itd fitclub-web-1 celery -A app worker -l info
-docker exec -itd fitclub-web-1 celery -A app beat -l info
+docker exec -itd fitclub_web_1 python manage.py bot
+docker exec -itd fitclub_web_1 celery -A app worker -l info
+docker exec -itd fitclub_web_1 celery -A app beat -l info
